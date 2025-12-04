f173daf9a8c4dc5bc078b13bbfd0609d386bdd91
JW Jacobson <116485484+jwjacobson@users.noreply.github.com>
2025-12-03 22:56:18 -0500

    Refactor app.js

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const NUM_SQUARES = 25;
 const SQUARES = document.querySelectorAll(".square")
-
+const BLINK_RATE = 200; // Rate of blink in milliseconds
+const ORIGINAL_COLOR = "#f3d2c1";
+const BLINK_COLOR = "#8bd3dd";
 
 function changeColor (square) {
-    square.style.backgroundColor = "#f582ae";
+    square.style.backgroundColor = BLINK_COLOR;
 }
 function revertColor (square) {
-    square.style.backgroundColor = "#f3d2c1"; 
+    square.style.backgroundColor = ORIGINAL_COLOR; 
 }
 
-function chooseSquare() {
+function blinkSquare() {
     let prev_idx;
     setInterval(() => {
         if (prev_idx !== undefined) {
@@ -18,7 +20,7 @@ function chooseSquare() {
         current_idx = Math.floor(Math.random() * NUM_SQUARES);
         changeColor(SQUARES[current_idx]);
         prev_idx = current_idx;
-    }, 200);
+    }, BLINK_RATE);
 }
 
-chooseSquare()
+blinkSquare()
